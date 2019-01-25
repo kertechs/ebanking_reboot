7897c5fb0a42c0b3bfdabc76e6b5a7a2ce14e8d8
Nassim Bennouna <nbennouna@kertechs.com>
2019-01-25 23:21:29 +0100

    Adding : - check for email unicity - flash messages success/errors - email notification

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -15,16 +15,56 @@ class InscriptionController extends AbstractController
     /**
      * @Route("/inscription", name="inscription")
      */
-    public function index(EntityManagerInterface $em, Request $request)
+    public function index(EntityManagerInterface $em, Request $request, \Swift_Mailer $mailer)
     {
         $form = $this->createForm(ClientRegistrationType::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $client = $form->getData();
-            $em->persist($client);
-            $em->flush();
 
-            $this->addFlash('success', 'Merci pour votre inscription');
+            $EmailAlreadyExist = $em->getRepository(Client::class)->createQueryBuilder('C')
+                ->where('C.email = :_ClientEmail')
+                ->andWhere('C.deletedAt IS NULL')
+                ->setParameter('_ClientEmail', $client->getEmail())
+                ->getQuery()
+                ->getResult();
+
+            if (!count($EmailAlreadyExist)) //Email not found
+            {
+                $em->persist($client);
+                $em->flush();
+
+                $this->addFlash('success', 'Merci pour votre inscription');
+
+                /*Notify the client*/
+                $message = (new \Swift_Message('Merci pour votre demande d\'inscription sur banquedauphine.online'))
+                    ->setFrom('contact@banquedauphine.online')
+                    ->setTo($client->getEmail())
+                    ->setBody(
+                        $this->renderView(
+                        // templates/emails/registration.html.twig
+                            'emails/registration.html.twig',[
+                                'client' => $client
+                            ]
+                        ),
+                        'text/html'
+                    )
+                    /*
+                     * If you also want to include a plaintext version of the message
+                    ->addPart(
+                        $this->renderView(
+                            'emails/registration.txt.twig',
+                            ['name' => $name]
+                        ),
+                        'text/plain'
+                    )
+                    */
+                ;
+                $mailer->send($message);
+            }
+            else {
+                $this->addFlash('error', 'Nous sommes désolés. Cette adresse email est déjà enregistrée');
+            }
 
             return $this->redirectToRoute('merci');
         }
@@ -42,7 +82,8 @@ class InscriptionController extends AbstractController
     {
         $flashbag = $this->get('session')->getFlashBag();
         $_success = $flashbag->peek('success');
-        if (! (is_array($_success) && count($_success)) ){
+        $_error = $flashbag->peek('error');
+        if (! (is_array($_success) && count($_success)) && ! (is_array($_error) && count($_error)) ){
             return $this->redirectToRoute('homepage');
         }
 
